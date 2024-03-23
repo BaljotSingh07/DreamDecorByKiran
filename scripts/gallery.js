4deed2a7f90c67373f87c2a0fde67b25fd4810d0
Baljot Singh <baljotvirk07@gmail.com>
2024-03-22 20:57:30 -0500

    small gallery fixes and max-width

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,27 +1,46 @@
+const tagToImageMap = {
+    jaggo : 'https://res.cloudinary.com/dngcd7taz/image/upload/v1711137645/samples/Pinder_Priya/ipkk3nai2qaximufplwu.jpg',
+    haldi : 'https://res.cloudinary.com/dngcd7taz/image/upload/v1711137644/samples/Pinder_Priya/boga0paxcwlqkfuolbax.jpg',
+    mehndi : 'https://res.cloudinary.com/dngcd7taz/image/upload/v1711137644/samples/Pinder_Priya/boga0paxcwlqkfuolbax.jpg',
+    wedding : 'https://res.cloudinary.com/dngcd7taz/image/upload/v1711124041/samples/Sukhu_Kanu/okfmnnajeec41xa2ziua.jpg',
+    reception : 'https://res.cloudinary.com/dngcd7taz/image/upload/v1711123476/samples/Sukhu_Kanu/axxsvcsn1xyaw05graiv.jpg', 
+}
+
+
 window.addEventListener('load', function () {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    var currentGallery = urlParams.get('list')
-    if(currentGallery == null){
-        currentGallery = "haldi";
-    }
-    const currentGalleryButton = document.querySelector(`[data-gallery-list='${currentGallery}']`);
-    currentGalleryButton.classList.toggle("active-button")
+    var myGallery;
+    var currentActiveButton = document.querySelector("[data-gallery-list='jaggo']");
+    const heroImage = document.getElementById('hero-image')
+    currentActiveButton.classList.toggle("active-button");
+    myGallery = renderGallery("jaggo")
 
     //add click handler on gallery buttons
     const galleryButtons = this.document.getElementById('gallery-buttons');
     galleryButtons.childNodes.forEach(button => {
         button.addEventListener('click', (b) => {
-            const buttonDataset = b.target.dataset.galleryList
-            window.location.href = `Gallery?list=${buttonDataset}`
-        })
-    });
+            //clear previous active button and destory previous gallery
+            // myGallery.destroy();
+            currentActiveButton.classList.toggle("active-button")
 
-    const myGallery= cloudinary.galleryWidget({
+            const buttonDataset = b.target.dataset.galleryList
+            heroImage.setAttribute('src', tagToImageMap[buttonDataset])
+            
+            currentActiveButton = document.querySelector(`[data-gallery-list='${buttonDataset}']`);
+            currentActiveButton.classList.toggle("active-button")
+            myGallery.update({
+                mediaAssets: [{ tag: buttonDataset }]
+              });
+        })
+        
+    });
+});
+
+function renderGallery(tag){
+    const gallery= cloudinary.galleryWidget({
         container: "#gallery-div",
         cloudName: "dngcd7taz",
         mediaAssets: [
-          {tag: currentGallery}, // by default mediaType: "image"
+          {tag: tag}, // by default mediaType: "image"
         ],
         aspectRatio: "1:1",
         displayProps: {
@@ -35,10 +54,11 @@ window.addEventListener('load', function () {
         },
         zoomProps: {
             "type": "popup",
-            "steps": 3
+            "steps": 3,
         },
-        
       });
       
-      myGallery.render();
-});
+      gallery.render();
+      return gallery;
+}
+
